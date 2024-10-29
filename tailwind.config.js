58348dba8c3d2537d545c278019b20b4c0801998
Nicola Manfredi <nicola.manfredi.210796@gmail.com>
2024-10-29 16:37:11 +0100

    tailwind ok

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heebo: ['Heebo', 'sans-serif'],
+        nunito: ['Nunito','sans-serif'],
+        pacifico: ["Pacifico", 'cursive']
+      },
+      colors: {
+        "accent-white": "#F1F8FF",
+        "accent-orange": "#FEA116",
+        "accent-black": "#0F172B"
+      }
+    },
+  },
+  plugins: [],
+}
