6cdfafabe5cd8b0134f539c8b658bd319006c2d4
Nicola Manfredi <nicola.manfredi.210796@gmail.com>
2024-10-30 17:38:37 +0100

    aggiunto effetto di lideUp quando una card entra nella viewport

--- src/app/components/service-cards/service-cards.component.ts
+++ src/app/components/service-cards/service-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { IconsService } from 'src/app/services/icons.service';
 import { ServicesService } from 'src/app/services/services.service';
 
@@ -7,12 +7,27 @@ import { ServicesService } from 'src/app/services/services.service';
   templateUrl: './service-cards.component.html',
   styleUrls: ['./service-cards.component.css']
 })
-export class ServiceCardsComponent {
+export class ServiceCardsComponent implements AfterViewInit{
   activeCardIndex: number = -1;
-
+  @ViewChildren('serviceCards') serviceCards!: QueryList<ElementRef>;
+  
   constructor(public iconsService:IconsService, public servicesService:ServicesService){}
-
+  
   setActiveCard(index:number){
     this.activeCardIndex = index;
+  }
+  
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate__animated');
+          entry.target.classList.add('animate__slideInUp');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+    
+    this.serviceCards.forEach((card) => observer.observe(card.nativeElement));
   }
 }
